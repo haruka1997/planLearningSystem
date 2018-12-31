8ada1f8e2ee48f1905de90f089387c72486f38f9
石川晴香 <haruka.ishikawa@avalon.co.jp>
2018-12-31 12:59:53 +0900

    学習記録の追加画面ビュー作成

---
+++ view/learningRecord.php
@@ -0,0 +1,48 @@
+<html>
+  <head>
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="./../lib/bootstrap.min.css">
+    <link rel="stylesheet" href="./../lib/propeller.min.css">
+
+    <link rel="stylesheet" href="./../css/main.css">
+    <link rel="stylesheet" href="./../css/planCreate.css">
+    <link rel="stylesheet" href="./../css/calender.css">
+    <link rel="stylesheet" href="./../css/planModal.css">
+    <link rel="stylesheet" href="./../css/analysisResultModal.css">
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./../script/planModal.js"></script>
+    <script type="text/javascript" src="./../script/analysisResultModal.js"></script>
+
+  </head>
+  <body>
+    <div class="mdl-layout mdl-js-layout">
+      <!-- ヘッダ -->
+      <?php include ('./header.php'); ?>
+      <!-- メニュー -->
+      <?php include ('./menu.php'); ?>
+      <!-- コンテンツ -->
+      <main class="mdl-layout__content">
+        <!-- 学習記録追加画面 -->
+        <div class="planCreate-content">
+            <!-- 計画実施率 -->
+            <div class="planProgress">
+                <div class="progress-rounded progress">
+                    <label class="progress-title">計画実施率：</label>
+                    <div class="progress-bar progress-bar-success" style="width: 34%;"></div>
+                    <label class="progress-persent">30%</label>
+                </div>
+            </div>
+            <!-- カレンダ -->
+            <?php include ('./calender.php'); ?>
+        </div>
+      </main>
+      <!-- 学習記録追加モーダル -->
+      <?php include ('./planCreateModal.php'); ?>
+      <!-- 学習記録詳細モーダル -->
+      <?php include ('./planDetailModal.php'); ?>
+    </div>
+  </body>
+</html>
--- view/menu.php
+++ view/menu.php
@@ -1,7 +1,8 @@
 <div class="mdl-layout__drawer">
     <span class="mdl-layout-title">メニュー</span>
     <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="./planCreate.php">学習計画と記録</a>
-        <a class="mdl-navigation__link" href="">学習履歴</a>
+        <a class="mdl-navigation__link" href="./planCreate.php">学習計画の作成</a>
+        <a class="mdl-navigation__link" href="./learningRecord.php">学習記録の追加</a>
+        <a class="mdl-navigation__link" href="">学習履歴の参照</a>
     </nav>
 </div>
