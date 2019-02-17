3ccb747a10a3077df638b0b43e658e105e8f5ee8
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-02-17 15:44:35 +0900

    学習の設定画面のビュー作成

--- view/planCreate/learningSetting.php
+++ view/planCreate/learningSetting.php
@@ -2,37 +2,38 @@
 <div class="analysis-form">
     <!-- 授業回 -->
     <div class="form-group pmd-textfield pmd-textfield-floating-label">       
-        <label>授業回を選択</label>
+        <label>学習する授業範囲を選択してください</label>
         <select class="select-simple form-control pmd-select2">
             <option></option>
-            <option>第1回</option>
-            <option>第2回</option>
-            <option>第3回</option>
-            <option>第4回</option>
-            <option>第5回</option>
-            <option>第6回</option>
-            <option>第7回</option>
-            <option>第8回</option>
-            <option>第9回</option>
-            <option>第10回</option>
-            <option>第11回</option>
-            <option>第12回</option>
-            <option>第13回</option>
+            <option>第1回 三角関数①（弧度法、基本性質、三角関数のグラフ）</option>
+            <option>第2回 三角関数②（三角方程式・不等式、加法定理、加法定理の応用）</option>
+            <option>第3回 指数関数①（指数法則、累乗根、指数関数のグラフ、指数の大小比較、指数方程式・不等式）</option>
+            <option>第4回 対数関数①（対数の基本性質、対数関数のグラフ、対数の大小比較、対数方程式・不等式、常用対数）</option>
+            <option>第5回 数列①（等差数列、等比数列、∑公式）</option>
+            <option>第6回 数列②（いろいろな数列、漸化式）</option>
+            <option>第7回 作問演習、TAとの個人面談</option>
+            <option>第8回 極限①（無限級数と極限）</option>
+            <option>第9回 極限②（極限の計算、初等関数の極限）</option>
+            <option>第10回 微分法①（微分係数、極限と導関数，接線の方程式，関数の増加・減少）</option>
+            <option>第11回 微分法②（積と商・合成関数の微分，初等関数の微分，微分法の応用）</option>
+            <option>第12回 積分法①（不定積分，定積分）</option>
+            <option>第13回 積分法②（面積）</option>
+            <option>第14回 総合演習 </option>
         </select>
     </div>
     <!-- 理解度 -->
     <div class="form-group pmd-textfield pmd-textfield-floating-label">       
-        <label>理解度を選択</label>
+        <label>授業範囲に対する自分の理解度を選択してください</label>
         <select class="select-simple form-control pmd-select2">
             <option></option>
-            <option>高</option>
-            <option>中</option>
-            <option>低</option>
+            <option>高い</option>
+            <option>普通</option>
+            <option>低い</option>
         </select>
     </div>
     <!-- 目標点数 -->
     <div class="form-group pmd-textfield pmd-textfield-floating-label">       
-        <label>目標点数を選択</label>
+        <label>事前テストの目標点数を選択してください</label>
         <select class="select-simple form-control pmd-select2">
             <option></option>
             <option>6点</option>
@@ -43,8 +44,8 @@
         </select>
     </div>
 </div>
-<div class="analysis-button">
+<div class="regist-button">
     <button class="mdl-button mdl-js-button mdl-button--raised">
-    <i class="material-icons">search</i> 分析
+     登録
     </button>
 </div>
