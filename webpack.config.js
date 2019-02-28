f742502a211e6f67b1db63625ce285c6d06f0879
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-02-28 16:46:37 +0900

    webpack環境設定

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+ 
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: `./src/index.js`,
+   
+    // ファイルの出力設定
+    output: {
+      // 出力ファイル名
+      filename: 'main.js'
+    },
+  };
+  
