9a5eed5ddae8b72d592744c743d65eae285a4d81
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-05-02 17:22:19 +0900

    webpackを使ってcalender.htmlを読み込み

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
 module.exports = {
  
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: `./src/index.js`,
+    entry: `./script/planCreate.js`,
+    mode: 'development',
    
     // ファイルの出力設定
     output: {
+      // path: './dist',
       // 出力ファイル名
-      filename: 'main.js'
+      filename: 'planCreate.js',
+      path: path.join(__dirname, 'dist')
     },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          loader: "html-loader"
+        }
+      ]
+    }
   };
   
