31a0c53b5e2d21a736036c4ff50de976374daa6c
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-05-15 16:46:49 +0900

    ログインのサーバ認証

---
+++ php/login.php
@@ -0,0 +1,18 @@
+<?php 
+    header("Content-Type: application/json; charset=UTF-8");
+    //エラー処理
+    try {
+        $dbh = new PDO('mysql:host=localhost; dbname=plan_learning_system', 'localhost', 'localhost');
+
+        $stmt = $dbh->prepare('SELECT * FROM user WHERE userId = :userId AND password = :password');    //入力したユーザIDかつパスワードの情報を選択
+        $stmt->bindParam(':userId', $_POST['userId'], PDO::PARAM_STR);
+        $stmt->bindParam(':password', $_POST['password'], PDO::PARAM_STR);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {    //入力したユーザIDとパスワードに一致するデータがあれば
+            echo json_encode($row);
+            exit();  // 処理終了
+        }
+    } catch (PDOException $e) {
+    }
+?>
--- script/login.js
+++ script/login.js
@@ -7,13 +7,29 @@ $(function(){
 
     // ログインボタンが押されたら
     $('.sign-button').click(function (){
-        
-        // ユーザ情報セッション保存
-        window.sessionStorage.setItem(['userId'], $('#userId'));
 
-        // 学習記録の作成ページに遷移
-        window.location.href = './../view/planCreate/index.php';
+        // ユーザ情報の認証
+        $.ajax({
+            url:'./../php/login.php',
+            type:'POST',
+            data:{
+                'userId':$('#userId').val(),
+                'password':$('#password').val()
+            },
+            dataType: 'json'       
+        })
+        // Ajaxリクエストが成功した時発動
+        .done( (data) => {
+            // ユーザ情報セッション保存
+            window.sessionStorage.setItem(['userId'], data.userId);
+            // 学習記録の作成ページに遷移
+            window.location.href = './../view/planCreate/index.php';
 
+        })
+        // Ajaxリクエストが失敗した時発動
+        .fail( (data) => {
+            alert('ログインに失敗しました');
+        })
 
     });
 });
