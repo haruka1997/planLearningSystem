a5de4a60730b610c7269c394c45ba950c7d53783
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-05-26 11:35:20 +0900

    settingIdをtrのidに設定

--- script/learningHistory.js
+++ script/learningHistory.js
@@ -6,13 +6,13 @@ var $ = modules.$; //jquery
 
 // テーブルの仮データ
 let tableDate = [
-    {'coverage': 1, 'executing': 50, 'goal': '達成', 'satisfaction': '満足している'},
-    {'coverage': 2, 'executing': 20, 'goal': '未達成', 'satisfaction': 'あまり満足していない'},
-    {'coverage': 3, 'executing': 30, 'goal': '達成', 'satisfaction': '満足している'},
-    {'coverage': 4, 'executing': 50, 'goal': '未達成', 'satisfaction': '満足していない'},
-    {'coverage': 5, 'executing': 10, 'goal': '達成', 'satisfaction': '満足している'},
-    {'coverage': 6, 'executing': 30, 'goal': '達成', 'satisfaction': '満足していない'},
-    {'coverage': 7, 'executing': 40, 'goal': '達成', 'satisfaction': '満足している'}
+    {'settingId': 'a', 'coverage': 1, 'executing': 50, 'goal': '達成', 'satisfaction': '満足している'},
+    {'settingId': 'b','coverage': 2, 'executing': 20, 'goal': '未達成', 'satisfaction': 'あまり満足していない'},
+    {'settingId': 'c','coverage': 3, 'executing': 30, 'goal': '達成', 'satisfaction': '満足している'},
+    {'settingId': 'd','coverage': 4, 'executing': 50, 'goal': '未達成', 'satisfaction': '満足していない'},
+    {'settingId': 'e','coverage': 5, 'executing': 10, 'goal': '達成', 'satisfaction': '満足している'},
+    {'settingId': 'f','coverage': 6, 'executing': 30, 'goal': '達成', 'satisfaction': '満足していない'},
+    {'settingId': 'g','coverage': 7, 'executing': 40, 'goal': '達成', 'satisfaction': '満足している'}
 ];
 
 $(function(){
@@ -21,9 +21,12 @@ $(function(){
 
     // 仮データ表示
     for(let i in tableDate){
-        $('.learning-history-tbody').append('<tr><td>' + tableDate[i].coverage + '回</td><td>' + tableDate[i].executing + '%</td><td>' + tableDate[i].goal + '</td><td>' + tableDate[i].satisfaction + '</td></tr>');
+        $('.learning-history-tbody').append('<tr id=' + tableDate[i].settingId + '><td>' + tableDate[i].coverage + '回</td><td>' + tableDate[i].executing + '%</td><td>' + tableDate[i].goal + '</td><td>' + tableDate[i].satisfaction + '</td></tr>');
     }
 
     // テーブル内を選択されたら
+    $('.learning-history-tbody tr').click(function (){
+        
+    });
     
 });
