2bb71589c4712946aad31a3011c5176f94e5257c
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-06-04 13:22:17 +0900

    カレンダーの縦幅を小さくする

--- script/module/initCalenderHtml.js
+++ script/module/initCalenderHtml.js
@@ -7,8 +7,8 @@ module.exports.init = function($, calenderDateArray){
     $(".calender-display-content").append(calender);
 
     // カレンダーの縦幅設定
-    var wH = $(window).height() - 50;  // 現在の画面の縦幅を取得
-    $('.calender-table tbody').css('height', wH);
+    // var wH = $(window).height() - 50;  // 現在の画面の縦幅を取得
+    // $('.calender-table tbody').css('height', wH);
 
     let day = ['(月)', '(火)', '(水)', '(木)', '(金)', '(土)', '(日)'];
     $('.calender-table tr .calender-date').each(function(i){
