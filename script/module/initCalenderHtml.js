221b3c59d00694218b41c73a2c5e7112093e126e
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-06-04 13:29:12 +0900

    カレンダーを更新しても表示位置を固定する

--- script/module/initCalenderHtml.js
+++ script/module/initCalenderHtml.js
@@ -1,15 +1,24 @@
 module.exports.init = function($, calenderDateArray){
+    let scrollTop = $('.calender tbody').scrollTop();   // 現在のカレンダーのスクロール位置を取得
+
+    // カレンダーがセットされていたら一度削除する
     if($(".calender-display-content").find('.calender')){
         $(".calender").remove();
     }
+
+    // カレンダーモジュール
     var calender = require('./../../view/common/calender.html');
 
+    // カレンダーをセットする
     $(".calender-display-content").append(calender);
+
+    $('.calender tbody').scrollTop(scrollTop);  // 前回のスクロール位置に設定
 
     // カレンダーの縦幅設定
     // var wH = $(window).height() - 50;  // 現在の画面の縦幅を取得
     // $('.calender-table tbody').css('height', wH);
 
+    // カレンダーの日付表示設定
     let day = ['(月)', '(火)', '(水)', '(木)', '(金)', '(土)', '(日)'];
     $('.calender-table tr .calender-date').each(function(i){
         $(this).text(calenderDateArray[i].date + '日' + day[i]);
