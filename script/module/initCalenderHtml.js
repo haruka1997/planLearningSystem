a6f4e0473d96c77708b268f753eccc63cc504558
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-06-04 16:29:34 +0900

    土曜なら青、日曜なら赤い文字にする

--- script/module/initCalenderHtml.js
+++ script/module/initCalenderHtml.js
@@ -26,6 +26,12 @@ module.exports.init = function($, calenderDateArray, selectButton){
     // カレンダーの日付表示設定
     $('.calender-table tr .calender-date').each(function(i){
         $(this).text(calenderDateArray[i].date + '日' + calenderDateArray[i].day);
+        if(calenderDateArray[i].day == '(土)'){
+            $(this).css('color', '#0288D1');
+        }
+        if(calenderDateArray[i].day == '(日)'){
+            $(this).css('color', '#F44336');
+        }
     });
 
 }
