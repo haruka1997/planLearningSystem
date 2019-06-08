68ee5ae9c69910e33960dff3bc254dc30b95c3ff
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-06-08 21:16:33 +0900

    htmlメソッドをtextに変更

--- script/module/calenderItemSet.js
+++ script/module/calenderItemSet.js
@@ -92,7 +92,7 @@ module.exports.set = function(displayItems, $, selectHistoryData, selectButton, 
 
             //予定を追加する対象行列に時間と予定名を追加
             // 学習内容7文字だけ抽出
-            $('.calender-table tbody tr:' + trNthChild[0] + ' td:' + tdNthChild).html(escape(items[itemsIndex].time.start) + ' ' + escape(items[itemsIndex].content));   //学習内容を設定
+            $('.calender-table tbody tr:' + trNthChild[0] + ' td:' + tdNthChild).text(escape(items[itemsIndex].time.start) + ' ' + escape(items[itemsIndex].content));   //学習内容を設定
            
             //行の削除
             for(var j=0; j<trNthChild.length; j++){ //色を塗る行分
