542bca206d33000be9dc3e5074a6e88cd6120fff
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-07-19 13:36:12 +0900

    履歴データの取得順を初期化

--- php/main/getHistoryData.php
+++ php/main/getHistoryData.php
@@ -11,7 +11,7 @@
         // MySQL ネイティブの静的プレースホルダを使用する
         $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $stmt = $dbh->prepare('SELECT * FROM history WHERE userId = :userId ORDER BY coverage ASC'); 
+        $stmt = $dbh->prepare('SELECT * FROM history WHERE userId = :userId'); 
         $stmt->bindParam(':userId', $_SESSION['userId'], PDO::PARAM_STR);
 
         $stmt->execute();
