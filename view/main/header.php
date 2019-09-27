99ff59853fda18a4e22b9e076915eccaf7cf33ca
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-09-27 18:27:22 +0900

    ヘルプページの作成

--- view/main/header.php
+++ view/main/header.php
@@ -3,6 +3,7 @@
     <span class="mdl-layout-title header-title-text">学習計画の作成</span>
     <div class="mdl-layout-spacer"></div>
     <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" target="_blank" href="./../help.html">ヘルプ</a>
       <a class="mdl-navigation__link" href="./../login.php">ログアウト</a>
     </nav>
   </div>
