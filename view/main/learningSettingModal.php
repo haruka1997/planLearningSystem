10c17fb01b10c511cc3695b7a13401b58cf0b351
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-09-27 16:27:49 +0900

    競合の解消

--- view/main/learningSettingModal.php
+++ view/main/learningSettingModal.php
@@ -12,23 +12,6 @@
             <!--授業回 -->
             <div class="form-group pmd-textfield">       
                 <label class="required">予習する授業範囲</label>
-<<<<<<< HEAD
-                <select class="select-simple form-control pmd-select2 required" id="coverage">
-                    <option value="1">第1回 命題論理・集合①（必要条件・十分条件，逆・裏・対偶，和集合，積集合，部分集合，集合が3つの場合の和集合と積集合，補集合，ド・モルガンの法則）</option>
-                    <option value="2">第2回 命題論理・集合②（応用問題）</option>
-                    <option value="3">第3回 命題論理・集合③（背理法，数学的帰納法）</option>
-                    <option value="4">第4回 命題論理・集合④（作問演習）</option>
-                    <option value="5">第5回 ベクトル①（ベクトルの計算，ベクトルの成分，ベクトルの内積）</option>
-                    <option value="6">第6回 ベクトル②（位置ベクトル）</option>
-                    <option value="7">第7回 ベクトル③（ベクトル方程式）</option>
-                    <option value="8">第8回 ベクトル④（ベクトルの応用，空間ベクトル①）</option>
-                    <option value="9">第9回 ベクトル⑤（空間ベクトル②）</option>
-                    <option value="10">第10回 ベクトル⑥（作問演習）</option>
-                    <option value="11">第11回 行列①（行列の相等と成分，行列の和・差・実数倍，零行列と列ベクトル行ベクトル，行列の積，対角行列と単位行列，行列の積の性質，ケーリー・ハミルトン）</option>
-                    <option value="12">第12回 行列②（逆行列，連立1次方程式の解法）</option>
-                    <option value="13">第13回 行列③（作問演習）</option>
-                    <option value="14">第14回 総合演習 </option>
-=======
                 <select class="select-simple form-control pmd-select2 required" id="coverage" disabled>
                     <option value="2" selected>第2回 順列①（和の法則，積の法則，順列の総数，順列の公式，円順列，重複順列）</option>
                     <option value="3">第3回 順列②（和の法則，積の法則，順列の応用問題）</option>
@@ -43,7 +26,6 @@
                     <option value="12">第12回 統計①（資料の整理、代表値）</option>
                     <option value="13">第13回 統計②（散らばりと相関係数）</option>
                     <option value="14">第14回 統計③（作問演習/個別面談） </option>
->>>>>>> fix/update-class-content
                     <option value="15">第15回 期末試験 </option>
                 </select>
             </div>
@@ -52,11 +34,7 @@
                 <label class="control-label required">
                     選択した授業日
                 </label>
-<<<<<<< HEAD
-                <input type="text" id="classDate" class="form-control required">
-=======
                 <input type="text" id="classDate" class="form-control required" value="2019-10-03">
->>>>>>> fix/update-class-content
                 <!-- <input type="date" id="classDate" class="form-control required"> -->
             </div>
            <!-- 理解度 -->
