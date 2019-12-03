c9d00bdccb20d99e7abf12c7ae4fed91b325c996
石川晴香 <haruka.ishikawa@avalon.co.jp>
2019-12-03 15:08:06 +0900

    授業回の更新(第11回)

--- view/main/learningSettingModal.php
+++ view/main/learningSettingModal.php
@@ -2,7 +2,7 @@
     <div class="plan-modal">
         <!-- ヘッダー -->
         <div id="modal-header">
-            <div class="header-title">目標の設定</div>
+            <div class="header-title">授業回の登録</div>
             <div class="header-cansel-button">×</div>
         </div>
         <!-- コンテンツ -->
@@ -19,10 +19,10 @@
                     <option value="5">第5回 組合せ①（組合せの公式，組み分け，同じものを含む順列，重複組合せ，二項定理）</option>
                     <option value="6">第6回 組合せ②（組合わせの応用問題）</option>
                     <option value="7">第7回 組合せ③（作問演習/個別面談）</option>
-                    <option value="8" selected>第8回 確率①（試行と事象，確率の定義，和事象と積事象の確率，加法定理，反復試行）</option>
+                    <option value="8">第8回 確率①（試行と事象，確率の定義，和事象と積事象の確率，加法定理，反復試行）</option>
                     <option value="9">第9回 確率②（期待値，応用問題）</option>
                     <option value="10">第10回 確率③（確率分布，二項分布）</option>
-                    <option value="11">第11回 確率④（作問演習/個別面談）</option>
+                    <option value="11" selected>第11回 確率④（作問演習/個別面談）</option>
                     <option value="12">第12回 統計①（資料の整理、代表値）</option>
                     <option value="13">第13回 統計②（散らばりと相関係数）</option>
                     <option value="14">第14回 統計③（作問演習/個別面談） </option>
@@ -34,7 +34,7 @@
                 <label class="control-label required">
                     選択した授業日
                 </label>
-                <input type="text" id="classDate" class="form-control required" value="2019-11-21">
+                <input type="text" id="classDate" class="form-control required" value="2019-12-12">
                 <!-- <input type="date" id="classDate" class="form-control required"> -->
             </div>
            <!-- 理解度 -->
