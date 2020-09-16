4b395d0b82ba098d65a23f1e159a6c8dcfc7b75b
石川晴香 <haruka.ishikawa@avalon.co.jp>
2020-09-16 13:33:28 +0900

    新規学習計画の作成ボタンの作成

--- view/main/index.php
+++ view/main/index.php
@@ -54,7 +54,9 @@
           </div>
           <!-- ボタン -->
           <div class="new-plan-create">
-            <button class="new-plan-create-button mdl-button mdl-js-button mdl-button--raised">授業回の登録</button>
+            <button class="new-plan-create-button mdl-button mdl-js-button mdl-button--raised">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/></svg>新規学習計画の作成
+          </button>
             <!-- <button class="chatbot-history-button mdl-button mdl-js-button mdl-button--raised">振り返り履歴</button> -->
           </div>
           <div class="calender-display-content">
